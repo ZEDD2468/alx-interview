dd7d7a7e6e4956e53493f1493fd95939d00eca1c
ZEDD2468 <victoradebayo360@gmail.com>
2023-08-30 19:55:51 +0100

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,25 +1,66 @@
 #!/usr/bin/python3
-"""Island perimeter computing module.
-"""
+""" Island Perimeter mandatory """
+
+
+def test0(n):
+    """
+    *******************************
+    ******test if n is 0 or 1******
+    *******************************
+    @n: number of the grid
+    Returns: 0 if n is 1 or 1 if n is 0
+    """
+    if (n == 0):
+        return 1
+    return 0
 
 
 def island_perimeter(grid):
-    """Computes the perimeter of an island with no lakes.
     """
-    perimeter = 0
-    if type(grid) != list:
-        return 0
-    n = len(grid)
-    for i, row in enumerate(grid):
-        m = len(row)
-        for j, cell in enumerate(row):
-            if cell == 0:
-                continue
-            edges = (
-                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
-                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
-                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
-                j == 0 or row[j - 1] == 0,
-            )
-            perimeter += sum(edges)
-    return perimeter
+    ***********************************************
+    ******returns the perimeter of the island******
+    ***************described in grid***************
+    ***********************************************
+    @grid: is a list of list of integers:
+                *** 0 represents water
+                *** 1 represents land
+                *** Each cell is square, with a side
+                    length of 1
+                *** Cells are connected horizontally/vertically
+                    (not diagonally).
+                *** grid is rectangular, with its width and
+                    height not exceeding 100
+            The grid is completely surrounded by water
+            There is only one island (or nothing).
+            The island doesn’t have “lakes” (water inside that isn’t
+                connected to the water surrounding the island).
+    """
+    row = len(grid)
+    col = len(grid[0])
+    assert (1 <= row and col <= 100), "length must be between 1 an 100"
+
+    p = 0
+    for i in range(row):
+        for j in range(col):
+            assert (grid[i][j] == 0) or (grid[i][j] == 1),\
+                                        "grid numbers must be 0 or 1"
+            if grid[i][j] == 1:
+                if i-1 < 0:
+                    p += 1
+                else:
+                    p += test0(grid[i-1][j])
+                if j-1 < 0:
+                    p += 1
+                else:
+                    p += test0(grid[i][j-1])
+
+                try:
+                    p += test0(grid[i+1][j])
+                except IndexError:
+                    p += 1
+                try:
+                    p += test0(grid[i][j+1])
+                except IndexError:
+                    p += 1
+
+    return p
